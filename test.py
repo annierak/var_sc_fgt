fbced01780fe8f7b28f9b0a507e7b178adcf8d3e
annierak <annie1rak@gmail.com>
2018-07-10 14:39:25 -0700

    working parts 1 and 2; not very fast

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ import matplotlib.pyplot as plt
 import sys
 import itertools
 from scipy.special import hermite
+import time
+import operator
+
+#For timing purposes
+counter = 0
 
 #Inputs
 
@@ -12,9 +17,12 @@ M = 100
 box_min = -2000
 box_max = 2000
 
-d = 3 #Dimension of space
+d = 2 #Dimension of space
 
+np.random.seed(1)
 t = np.random.uniform(box_min,box_max,(M,d))
+
+p = 24
 
 #Scale t to unit box
 t = (t-box_min)/(box_max-box_min)
@@ -24,6 +32,7 @@ t = (t-box_min)/(box_max-box_min)
 #N sources s_j
 N = 100
 
+np.random.seed(1)
 s = np.random.uniform(box_min,box_max,(N,d))
 #Scale s to unit box
 s = (s-box_min)/(box_max-box_min)
@@ -42,7 +51,7 @@ f = np.random.uniform(1,200,(N,1))
 #initial puff rad = 0.01 ==> delta_min = 0.0002
 a = (0.0001 +  1./N)#/(box_max-box_min)
 b = 0.1
-epsilon = 1e-8
+epsilon = 1e-6
 
 
 #utility functions
@@ -50,19 +59,15 @@ def assign_box(location,bounds,width):
     #Given outer bounds on x, y, z (large box), box width, return
     #x, y, z coordinates of the box the location is in
     #location: [x,y,z]
-    #bounds : [xmin,xmax,ymin,ymax,zmin,zmax]
-    if (bounds[0]<=location[0]<=bounds[1])& \
-        (bounds[2]<=location[1]<=bounds[3])& \
-        (bounds[4]<=location[2]<=bounds[5]):
-        pass
-    else:
-        print('Box assignment error: provided location not in big box')
-        sys.exit()
-    x_box_coord = int(np.floor((location[0]-bounds[0])/width))
-    y_box_coord = int(np.floor((location[1]-bounds[2])/width))
-    z_box_coord = int(np.floor((location[2]-bounds[4])/width))
-
-    return x_box_coord,y_box_coord,z_box_coord
+    #bounds : array([[xmin,xmax],[ymin,ymax],[zmin,zmax]]) etc
+    box_coords = np.zeros(len(location))
+    for dim in range(len(location)):
+        if (bounds[dim,0]<=location[dim]<=bounds[dim,1]):
+            box_coords[dim] = int(np.floor((location[dim]-bounds[dim,0])/width))
+        else:
+            print('Box assignment error: provided location not in big box')
+            sys.exit()
+    return box_coords
 
 def distance(p1,p2):
     #N-d distance between two points
@@ -91,15 +96,28 @@ def nd_abs(alpha):
     return sum(alpha[i] for i in range(len(alpha)))
 def nd_factorial(alpha):
     elements = [math.factorial(alpha[i]) for i in range(len(alpha))]
-    return np.prod(elements)
+    return reduce(operator.mul,elements,1)
 def nd_exp(base,alpha):
     elements = [base[i]**alpha[i] for i in range(len(alpha))]
-    return np.prod(elements)
-def evaluate_nd_hermite_function(alpha,t):
-    element_hermite_polynomials = [hermite(alpha[i])(t[i]) for i in range(len(alpha)) ]
-    return np.exp(-1*(nd_abs(t)**2))*np.prod(element_hermite_polynomials)
+    return reduce(operator.mul,elements,1)
 
-
+# def evaluate_nd_hermite_function(alpha,t):
+#     last = time.time()
+#     element_hermite_polynomials = [hermite(alpha[i])(t[i]) for i in range(len(alpha)) ]
+#     output = np.exp(-1*(nd_abs(t)**2))*np.prod(element_hermite_polynomials)
+#     print(time.time()-last)
+#     return output
+def evaluate_nd_hermite_function(H_beta_is,t,coeff):
+    global counter
+    # last = time.time()
+    element_hermite_polynomials = [H_beta_is[i](t[i]) for i in range(len(t)) ]
+    # print('eval time',time.time()-last);last = time.time()
+    # output = coeff*np.prod(element_hermite_polynomials)
+    output = coeff*reduce(operator.mul,element_hermite_polynomials,1)
+    # output = coeff*element_hermite_polynomials[0]*element_hermite_polynomials[1]
+    # print('sum time',time.time()-last)
+    # counter+=1
+    return output
 # print box_distance_check([2,4,9,5],[1,3,4,5],1)# print assign_box([4.7,9.1,29.1],[0,27,0,27,0,27],3)
 
 class TaylorExpansion(object):
@@ -114,27 +132,44 @@ class TaylorExpansion(object):
         self.delta_j_set=delta_j_set
         self.s_set=s_set
         self.indexer = [x for x in nd_itertool(self.d,self.p)]
+        last = time.time()
         self.C_betas = np.zeros(tuple([p for i in range(d)]))
+        #This piece is for obtaining hermite function from hermite polynomial
+        #precomputed for efficiency
+        #These are indexed by the sources
+        self.hermite_coeffs = [np.exp(-1*(nd_abs(
+        (s_j-self.t_0)/(math.sqrt(delta_j)))**2))
+         for delta_j,s_j in list(zip(self.delta_j_set,self.s_set))]
         for beta in self.indexer:
             indices = tuple([el-1 for el in beta])
             self.C_betas[indices]=self.compute_C_beta(beta)
+        print('time computing C_betas: '+str(time.time()-last))
     def compute_C_beta(self,beta):
-        running_sum = 0
-        for delta_j,s_j in list(zip(self.delta_j_set,self.s_set)):
-            h_beta = evaluate_nd_hermite_function(
-                beta,((s_j-self.t_0)/(math.sqrt(delta_j))))
-            running_sum += h_beta*(self.delta/delta_j)**(nd_abs(beta)/2)
-        return (1./(nd_factorial(beta)))*running_sum
+        #First, create hermite objects for the 1D components H_beta_1(), H_beta_2(), etc
+        H_beta_is = [hermite(beta_i) for beta_i in beta]
+        #Then, make a list of the summands:
+        #the evaluted h_beta for each source (indexed by j),
+        #times the delta fraction coefficient
+        summands = [(self.delta/delta_j)**(
+            nd_abs(beta)/2)*evaluate_nd_hermite_function(H_beta_is,
+            ((s_j-self.t_0)/(math.sqrt(delta_j
+            ))),hermite_coeff) for delta_j,s_j,hermite_coeff in list(zip(
+            self.delta_j_set,self.s_set,self.hermite_coeffs))]
+        output = (1./(nd_factorial(beta)))*np.sum(summands)
+        return output
     def evaluate(self,t):
-        value = 0
+        running_sum = 0
         for beta in self.indexer:
-            value += self.C_betas[beta]*nd_exp(((t-self.t_0)/(math.sqrt(
+            # last = time.time()
+            indices = tuple([el-1 for el in beta])
+            running_sum += self.C_betas[indices]*nd_exp(((t-self.t_0)/(math.sqrt(
                 self.delta))),beta)
-        return value
+            # print(time.time()-last)
+        return running_sum
 
 class TargetValueCollector(object):
     '''Object class for storing target values in Gauss transform'''
-    def __init__(self,M,N,t,s,f,epsilon,delta,a,b,d):
+    def __init__(self,M,N,t,s,f,epsilon,delta,a,b,d,p):
         self.Sf = np.zeros(M) #Summed target values
         self.t = t #target positions
         self.s = s #source positions
@@ -143,10 +178,12 @@ class TargetValueCollector(object):
         self.delta = delta #source deltas
         self.a = a
         self.d = d #dimension of space
+        self.p = p
         self.group_1_indices = np.where(delta<=a)[0]
         self.group_2_indices = np.where((delta>a)&(delta<b))[0]
         self.group_3_indices = np.where(delta>b)[0]
     def update_group_1(self):
+        last = time.time()
         #Process for group 1 of the sources: 0 \leq \delta \leq a
         #For all of the sources in this group, add their contribution to every target
         group_1_sources = self.s[self.group_1_indices,:]
@@ -155,9 +192,11 @@ class TargetValueCollector(object):
         box_width = 1/n_boxes
         #We create a subdivision of the box [0,1]^3 into boxes of length R
         #Obtain a list of box locations for every target
-        target_boxes = [assign_box(target,[0,1,0,1,0,1],box_width) for target in self.t]
+        bounds = np.array([[0,1] for i in range(d)])
+        print(bounds)
+        target_boxes = [assign_box(target,bounds,box_width) for target in self.t]
         #And for every source
-        source_boxes = [assign_box(source,[0,1,0,1,0,1],box_width) for source in group_1_sources]
+        source_boxes = [assign_box(source,bounds,box_width) for source in group_1_sources]
         #Loop through the sources
         for source_index in range(len(group_1_sources)):
         #Find all the boxes 1 box away from the source's box (by looping through targets)
@@ -171,23 +210,27 @@ class TargetValueCollector(object):
                     self.t[target_index],
                     self.s[self.group_1_indices[source_index]],
                     self.delta[self.group_1_indices[source_index]])
+        print('Time updating group 1:'+str(time.time()-last))
     # def update_group_2(self):
     #     #Process for group 2 of the sources: a < \delta < b
     #     #For all of the sources in this group, add their contribution to every target
     def update_group_3(self):
+        last = time.time()
         #Process for group 3 of the sources: b \leq \delta
         #Create a Taylor polynomial encompassing all sources in this group
         #add the value of this polynomial at the target to every target
-        t_0 = (.5,.5,.5)
+        t_0 = tuple([.5 for i in range(d)])
         delta_j_set = self.delta[self.group_3_indices]
         s_set = self.s[self.group_3_indices]
         delta = min(delta_j_set) #in the category
-        p = 20
-        taylorExpansion = TaylorExpansion(t_0,delta,p,delta_j_set,s_set,self.d)
-        for target_index in range(len(group_1_sources)):
+        taylorExpansion = TaylorExpansion(t_0,delta,self.p,delta_j_set,s_set,self.d)
+        group_3_sources = self.s[self.group_3_indices,:]
+        for target_index in range(len(self.t)):
             self.Sf[target_index]+=taylorExpansion.evaluate(self.t[target_index])
+        print('Time updating group 3:'+str(time.time()-last))
 
-targetCollector = TargetValueCollector(M,N,t,s,f,epsilon,delta,a,b,d)
+targetCollector = TargetValueCollector(M,N,t,s,f,epsilon,delta,a,b,d,p)
 targetCollector.update_group_1()
 targetCollector.update_group_3()
+print(counter)
 # targetCollector.update_group_2()
